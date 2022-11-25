f48fc17c8c918dd45dbe180d4acb76196fb46c07
DESKTOP-T1P4KAC\User <aspeeda@yandex.ru>
2022-11-26 00:48:31 +0500

    Add test Forms

---
+++ src/test/java/testForms.java
@@ -0,0 +1,54 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class testForms {
+    String name = "Ivan";
+    String surname = "Ivanov";
+    String email = "example@example.ru";
+    String phNumber = "8123321789";
+    String subject = "English";
+    String address = "city, street, house 1";
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void successfulRegistrationTest() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue(name);
+        $("#lastName").setValue(surname);
+        $("#userEmail").setValue(email);
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue(phNumber);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("December");
+        $(".react-datepicker__year-select").selectOption("1989");
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue(subject).pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/1.jpg");
+        $("#currentAddress").setValue(address);
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+        $(".modal-dialog").should(appear);
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text(name), text(surname),
+                text(email), text(phNumber));
+    }
+}
